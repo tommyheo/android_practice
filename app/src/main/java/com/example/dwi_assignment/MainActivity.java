0829221ff6e587b8bbefa88aa6edda42e6e2dc94
tommyheo <jyhuh8775@naver.com>
2025-08-13 00:02:01 +0900

    fix: 버튼 클릭 시 버튼 text 변경

--- app/src/main/java/com/example/dwi_assignment/MainActivity.java
+++ app/src/main/java/com/example/dwi_assignment/MainActivity.java
@@ -39,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Log.d(TAG, "버튼이 클릭 되었음");
                 textView.setText("Welcome!!!");
+                button.setText("Well Done!!!");
             }
         });
     }
